0824d00c3556d9bd2af138fd8149712911bf08d3
Ty Busby <tybusby@gmail.com>
2026-03-06 11:59:20 -0500

    remove temp file save

--- src/agents/motion.ts
+++ src/agents/motion.ts
@@ -7,9 +7,6 @@ import { Label } from "../tasks/draw/label";
 import { Trail } from "../tasks/draw/trail";
 import { Watermark } from "../tasks/draw/watermark";
 import { Drawing } from "../tasks/draw";
-import { Throttle } from "../tasks/filter/throttle";
-import { SaveImage } from "../tasks/transfer/save";
-import { DatePath } from "../paths/date";
 import { Ignore } from "../tasks/filter/ignore";
 
 export class Motion extends Agent {
@@ -32,10 +29,6 @@ export class Motion extends Agent {
         this.addTask(new Ignore(1740, 562, 20));
         this.addTask(drawing);
         this.addTask(viewer);
-
-        const filepath = new DatePath("/Users/tybusby/Camera/live", "motion");
-        this.addTask(new Throttle(0.1));
-        this.addTask(new SaveImage(filepath));
     }
 
     public override toString(): string {
