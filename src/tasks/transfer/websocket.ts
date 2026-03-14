38904eef426ec4fd3f29edd3ad68c1d02fc397e9
Ty Busby <tybusby@gmail.com>
2026-03-13 20:29:47 -0400

    add running check

--- src/tasks/transfer/websocket.ts
+++ src/tasks/transfer/websocket.ts
@@ -11,6 +11,8 @@ export class WebSocketTask extends WebSocketSession implements ImageTask {
     }
 
     public async process(frame: ImageFrame): Promise<ImageFrame | null> {
+        if (!this.running) return frame;
+
         this.send(frame.image.buffer);
 
         return frame;
